aee01dd160eaacb72dd974fe1a676f7b41092e59
sirRoach-nfp <nathanieljosepascual.psjnhs@gmail.com>
2024-07-12 18:07:37 +0800

    change port

--- backEnd/index.js
+++ backEnd/index.js
@@ -40,6 +40,7 @@ app.use("/lama",(req,res) =>{
 })
 
 
-app.listen("5000", () =>{
-    console.log("backend starts")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
